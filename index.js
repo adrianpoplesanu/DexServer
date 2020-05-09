55e8d2982fa563e3d1a40f54a62a45bd34524924
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-05-09 18:43:21 +0300

    login connection functionality added

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 const WebSocket = require('ws');
 var port = 8092;
+console.log("started Dexiciul v0.2...");
 
-console.log("running Dexiciul v0.1...");
-console.log("listening on port: " + port)
+var wss = new WebSocket.Server({port: port})
+console.log("listening on port: " + port);
+
+var connections = [];
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        if (message == "check connection still active...") {
+            // do nothing, it's just an echo from the client to test the connection is still up
+            return
+        }
+        var data = JSON.parse(message);
+        if (data['operation'] == 'connect') {
+            var username = data['data'].username;
+            var password = data['data'].password;
+            if (username == 'adrianus' && password == "dexiciul") {
+                var packet = {'operation': 'login-success', 'username': 'adrianus'}
+                ws.chat_settings['username'] = 'adrianus';
+                ws.send(JSON.stringify(packet));
+                console.log('adrianus connected');
+                return;
+            }
+            if (username = 'ramonix' && password == "dexiciut") {
+                var packet = {'operation': 'login-success', 'username': 'ramonix'}
+                ws.chat_settings['username'] = 'ramonix';
+                ws.send(JSON.stringify(packet));
+                console.log('ramonix connected');
+                return;
+            }
+            ws.send('wrong username or passowrd');
+        }
+    });
+
+    ws.on('close', ws => {
+        console.log("websocket onclose");
+    });
+
+    // this is the so called "onConnect" handler
+    ws.chat_settings = {};
+    connections.push(ws);
+});
